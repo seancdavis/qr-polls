23728816bba6e286eac2fd835caebdff490651fa
Sean C Davis <scdavis41@gmail.com>
2024-12-25 07:53:23 -0500

    Supabase client only available to nitro

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const { data } = await useFetch("/api/polls/1");
+</script>
+
 <template>
   <p class="text-3xl font-bold underline">Hello World</p>
+  <pre>{{ data }}</pre>
 </template>
--- server/utils/supabase.ts
+++ server/utils/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../types/supabase";
+import type { Database } from "../../shared/types/supabase";
 
 export default createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_API_KEY!);
