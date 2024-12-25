81c1cef1bd855197891e4a1fd0b49006dd39c7f3
Sean C Davis <scdavis41@gmail.com>
2024-12-25 07:51:06 -0500

    API route that returns poll details

---
+++ server/api/polls/[id].ts
@@ -0,0 +1,44 @@
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, "id");
+
+  if (!id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Poll ID is required",
+    });
+  }
+
+  const { data, error } = await supabase
+    .from("polls")
+    .select(
+      `
+        id,
+        title,
+        created_at,
+        questions (
+          id,
+          title,
+          created_at,
+          poll_id,
+          votes (
+            id,
+            created_at,
+            session_id,
+            question_id
+          )
+        )
+      `,
+    )
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Poll not found",
+    });
+  }
+
+  return { id, data };
+});
