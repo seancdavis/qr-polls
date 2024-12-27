19d3494a0dff580c592436c9ad06bfb83674e8a9
Sean C Davis <scdavis41@gmail.com>
2024-12-27 12:03:27 -0500

    Enable debug mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-12-24",
   devtools: { enabled: true },
@@ -6,4 +5,5 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   modules: ["@nuxtjs/tailwindcss"],
+  debug: true,
 });
