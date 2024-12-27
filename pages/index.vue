313bc3b8927af8a53b0eafe36ce39b0ffb7b7587
Sean C Davis <scdavis41@gmail.com>
2024-12-27 07:05:57 -0500

    Add link to home page instead of the details

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
-<script setup lang="ts">
-const { data } = await useFetch("/api/polls/9387ee29-81a1-4f1e-b979-b31d8caade26");
-</script>
-
 <template>
-  <p class="text-3xl font-bold underline">Hello World</p>
-  <pre>{{ data }}</pre>
+  <h1 class="text-3xl font-bold underline">QR Poll Demo</h1>
+  <p><a href="/polls/9387ee29-81a1-4f1e-b979-b31d8caade26">View an example poll</a></p>
 </template>
