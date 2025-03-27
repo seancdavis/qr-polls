f065fbb016b8693823560878d17d450ca0e231bb
Sean C Davis <scdavis41@gmail.com>
2025-03-27 13:54:34 -0400

    Cache test page

---
+++ pages/test.vue
@@ -0,0 +1,10 @@
+<script setup>
+// pages/test.vue
+const header = useResponseHeader("X-My-Header");
+header.value = "my-value";
+</script>
+
+<template>
+  <h1>Test page with custom header</h1>
+  <p>The response from the server for this "/test" page will have a custom "X-My-Header" header.</p>
+</template>
